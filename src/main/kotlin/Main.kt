44775dd60c9663c361781ba7c974a4827584d0d4
fedorovaea18 <fedorovaea18@yandex.ru>
2025-02-20 19:18:24 +0300

    KLB-02: added file words.txt with 3 english words with translation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package ru.fedorova.spring
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+
+    val wordsFile: File = File("words.txt")
+    wordsFile.createNewFile()
+    val lines = wordsFile.readLines()
+
+    for (line in lines) {
+        println(line)
+    }
+
 }
