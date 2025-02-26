a2623f4f0dac32b1a71f1f3a960534275b1ade72
fedorovaea18 <fedorovaea18@yandex.ru>
2025-02-26 15:45:14 +0300

    KLB-02: added data class Word with fields and mutable list(dictionary) for adding and storing words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,27 @@ package ru.fedorova.spring
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translation: String,
+    val correctAnswersCount: Int = 0,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.forEachLine { println(it) }
+
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    //wordsFile.createNewFile()
+
+    wordsFile.forEachLine { line ->
+        val part = line.split("|")
+        val correctAnswersCount = part[2].toIntOrNull() ?: 0
+        val word = Word(original = part[0], translation = part[1], correctAnswersCount = correctAnswersCount)
+        dictionary.add(word)
+    }
+
+    println(dictionary)
 
 }
